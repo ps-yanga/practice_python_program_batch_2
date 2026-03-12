d9d5b9e437063e922981a83ae79ec54549faa481
ps-yanga <yangaprincess03@gmail.com>
2026-03-13 00:03:38 +0800

    batch 1

---
+++ Batch 1/product_of_two_numbers_p4.py
@@ -0,0 +1,7 @@
+# multiplication
+# number 1
+num1 = int(input("Enter a number: "))
+# number 2
+num2 = int(input("Enter a number: "))
+# product
+print("THE PRODUCT OF TWO NUMBERS IS", num1 * num2)
