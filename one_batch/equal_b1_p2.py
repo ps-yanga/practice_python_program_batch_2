1c5f906be3da8006dfe2cc1f84d83286d0acc5de
ps-yanga <yangaprincess03@gmail.com>
2026-03-13 00:51:35 +0800

    batch 1

---
+++ one_batch/equal_b1_p2.py
@@ -0,0 +1,11 @@
+# number 1
+num1 = int(input("enter first number: "))
+# number 2
+num2 = int(input("enter second number: "))
+
+if num1 == num2:
+    print("equal")
+elif num1 > num2:
+    print(num1)
+else:
+    print(num2)
