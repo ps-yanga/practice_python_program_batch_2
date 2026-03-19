f1ac57bc78f6fbb728f02a701717539ae30a394f
ps-yanga <yangaprincess03@gmail.com>
2026-03-19 23:41:52 +0800

    first entry

--- three/first_entry_b3_p2.py
+++ three/first_entry_b3_p2.py
@@ -1,2 +1,7 @@
 # # Prog02: Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
-
+numbers = []
+for i in range(10):
+    num = int(input("Enter number: "))
+    numbers.append(num)
+print("ALL NUMBERS:",*numbers)
+print("NO DUPLICATE:",*dict.fromkeys(numbers))
