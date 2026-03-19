82b915f6d50e65fb6fb6a9fc30ec9e78dc728763
ps-yanga <yangaprincess03@gmail.com>
2026-03-20 02:10:20 +0800

    average

--- four/average_b4_p5.py
+++ four/average_b4_p5.py
@@ -1,1 +1,9 @@
-Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average
+# Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average
+numbers = []
+while True:
+    num = input("Enter number: ")
+    if not num.isdigit():
+        break
+    numbers.append(int(num))
+avg = sum(numbers) / len(numbers)
+print(avg if numbers else 0)
