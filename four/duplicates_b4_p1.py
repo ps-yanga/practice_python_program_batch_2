3277e015e06edf65a8dfc896c4630eda453acb2d
ps-yanga <yangaprincess03@gmail.com>
2026-03-20 01:26:13 +0800

    duplicates '

--- four/duplicates_b4_p1.py
+++ four/duplicates_b4_p1.py
@@ -1,1 +1,6 @@
 # Prog01: Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
+numbers = []
+for i in range(10):
+    numbers.append(int(input("Enter number: ")))
+dups = {n for n in numbers if numbers.count(n) > 1}
+print("Duplicates:", *dups)
