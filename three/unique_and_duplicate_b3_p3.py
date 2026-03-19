701914fba37ce46cdbedde55fe164028599e1c64
ps-yanga <yangaprincess03@gmail.com>
2026-03-20 00:09:45 +0800

    unique and dups

--- three/unique_and_duplicate_b3_p3.py
+++ three/unique_and_duplicate_b3_p3.py
@@ -1,1 +1,12 @@
 # Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display "Unique" after input when the inputted number don't have duplicate. Display "Duplicate" after input when the inputted number have duplicate.
+numbers = []
+while True:
+    inp = input("Enter number: ")
+    if not inp.isdigit():
+        break
+    num = int(inp)
+    if num in numbers:
+        print("Duplicate")
+    else:
+        print("Unique")
+        numbers.append(num)
