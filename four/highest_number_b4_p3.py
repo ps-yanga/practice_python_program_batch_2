e3894f27c400e72ea645ed74cdaa0fa71bbbaafd
ps-yanga <yangaprincess03@gmail.com>
2026-03-20 01:46:43 +0800

    highest number

--- four/highest_number_b4_p3.py
+++ four/highest_number_b4_p3.py
@@ -1,1 +1,8 @@
 # Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the highest number
+numbers = []
+while True:
+    inp = input("Enter number: ")
+    if not inp.isdigit():
+        break
+    numbers.append(int(inp))
+print(max(numbers) if numbers else "No numbers")
