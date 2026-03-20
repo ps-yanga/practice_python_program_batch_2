8ced8038ff671cd15e65f6dfb43e4a152ba72e04
ps-yanga <yangaprincess03@gmail.com>
2026-03-20 16:43:07 +0800

    final

--- five/all_capital_letter_b5_p3.py
+++ five/all_capital_letter_b5_p3.py
@@ -1,4 +1,3 @@
-# Prog03: Create a program that ask the user to input their fullname. Print the input in all capital letter.
-# Example:
-# Input: Juan Dela Cruz
-# Output: JUAN DELA CRUZ
+name=input("enter your fullname:")
+fullname=name.upper()
+print(fullname)
