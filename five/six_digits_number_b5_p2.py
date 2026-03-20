9070b400bccd6b9cb6f11d86ad08c69eec56b755
ps-yanga <yangaprincess03@gmail.com>
2026-03-20 16:17:52 +0800

    format

--- five/six_digits_number_b5_p2.py
+++ five/six_digits_number_b5_p2.py
@@ -1,4 +1,3 @@
-# Prog02: Create a program that ask the user to input a number (0-1000). Print the number in 6 digit format. Add zeros at the beginning to complete the 6 digit.
-# Example:
-# Input: 143
-# Output: 00014
+num = int(input("Enter a number (0-1000): "))
+numbers=str(num).zfill(6)
+print(numbers)
