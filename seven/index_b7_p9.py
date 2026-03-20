8e48dcc8b2164bf68135993b9cd1518f1bcbb9a8
ps-yanga <yangaprincess03@gmail.com>
2026-03-20 19:25:36 +0800

    last

---
+++ seven/index_b7_p9.py
@@ -0,0 +1,13 @@
+word=input("Enter a word: ")
+substr=input("Enter a substring: ")
+pos=0
+found=False
+for i in range(len(word)):
+    if word[i:i*len(substr)]==substr:
+        pos=i
+        found=True
+        break
+if found:
+    print(pos)
+else:
+    print("Not found")
